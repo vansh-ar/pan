d47b30c0283498fe7d60c045638916f1fd7f3eb8
vansh-ar <73380365+vansh-ar@users.noreply.github.com>
2021-01-14 19:47:55 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,18 @@
+class Bob{
+    constructor(bodyA,bodyB){
+        var options= {
+            bodyA:bodyA,
+            bodyB:bodyB,
+            lenght:10,
+            stiffness:0.04
+        } 
+        this.chain=Constraint.create(options);
+        World.add(world,this.chain)
+    }
+    display(){
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
